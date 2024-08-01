7eaa417f88100e60e0d6f782003af46fd8f786b4
sudhanshu choudhary <sudhanshuchoudhary0405@gmail.com>
2024-08-01 08:28:34 +0530

    successfully connected to database

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js" 
+
+async function connectDB(){
+  try {
+    const connectionObj = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    console.log(connectionObj.connection.host);
+  } catch (error) {
+    console.log("something went wrong")
+    process.exit(1);
+  }
+  
+}
+
+export default connectDB
